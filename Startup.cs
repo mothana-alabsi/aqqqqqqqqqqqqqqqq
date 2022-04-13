f2c6a062402835c415efea525378fae8d2183267
mothana-alabsi <75069561+mothana-alabsi@users.noreply.github.com>
2022-04-13 15:05:40 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,189 @@
+////using Microsoft.AspNetCore.Builder;
+////using Microsoft.AspNetCore.Hosting;
+////using Microsoft.AspNetCore.HttpsPolicy;
+////using Microsoft.AspNetCore.Mvc;
+////using Microsoft.EntityFrameworkCore;
+////using Microsoft.Extensions.Configuration;
+////using Microsoft.Extensions.DependencyInjection;
+////using Microsoft.Extensions.Hosting;
+////using Microsoft.Extensions.Logging;
+////using Microsoft.OpenApi.Models;
+////using Qanati_Explore.Models;
+////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Threading.Tasks;
+
+////namespace Qanati_Explore
+////{
+////    public class Startup
+////    {
+////        public Startup(IConfiguration configuration)
+////        {
+////            Configuration = configuration;
+////        }
+
+////        public IConfiguration Configuration { get; }
+
+////        // This method gets called by the runtime. Use this method to add services to the container.
+////        public void ConfigureServices(IServiceCollection services)
+////        {
+
+////            services.AddControllers();
+////            services.AddMvc();
+////            services.AddDbContext<DataContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+////            services.AddCors();
+
+
+////        }
+
+////        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+////        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+////        {
+////            if (env.IsDevelopment())
+////            {
+////                app.UseDeveloperExceptionPage();
+
+////            }
+////            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+////            app.UseHttpsRedirection();
+
+
+
+////            app.UseRouting();
+
+////            app.UseAuthorization();
+
+////            app.UseEndpoints(endpoints =>
+////            {
+////                endpoints.MapControllers();
+
+////            });
+////        }
+////    }
+////}
+
+
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.HttpsPolicy;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.Extensions.Logging;
+//using Microsoft.OpenApi.Models;
+//using Qanati_Explore.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace Qanati_Explore
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+
+//        public IConfiguration Configuration { get; }
+
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddControllers();
+//            services.AddDbContext<DataContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+//        }
+
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//            }
+//            else
+//            {
+//                app.UseHsts();
+//            }
+//            app.UseHttpsRedirection();
+
+//            app.UseAuthorization();
+
+
+
+//        }
+//    }
+//}
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Qanati_Explore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Qanati_Explore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+              services.AddDbContext<DataContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication8", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication8 v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
+
+
